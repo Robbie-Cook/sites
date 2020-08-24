a3025d1fe706e6959a03fbb96f55f51dda9f4e87
Robbie Cook <robbieck7@gmail.com>
2020-08-24 20:08:08 +1200

    Started blog page

--- packages/gatsby-theme-cara/gatsby-node.js
+++ packages/gatsby-theme-cara/gatsby-node.js
@@ -1,12 +1,17 @@
-const standardBasePath = `/`
+const standardBasePath = `/`;
+const path = require('path');
 
 exports.createPages = async ({ actions }, themeOptions) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const basePath = themeOptions.basePath || standardBasePath
+  const basePath = themeOptions.basePath || standardBasePath;
 
   createPage({
     path: basePath,
     component: require.resolve(`./src/templates/cara.tsx`),
-  })
-}
+  });
+  createPage({
+    path: path.resolve(basePath, 'blog'),
+    component: require.resolve(`./src/templates/blog.tsx`),
+  });
+};
