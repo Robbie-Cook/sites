abeee3f616ec4cbcc566e47e60fe47ada3e8e4ff
Robbie Cook <robbieck7@gmail.com>
2020-08-24 21:55:51 +1200

    Filling out post

--- packages/gatsby-theme-cara/src/components/blog/Post.tsx
+++ packages/gatsby-theme-cara/src/components/blog/Post.tsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
-import { jsx, Styled, css, ThemeProvider } from "theme-ui";
+import { Styled, ThemeProvider } from "theme-ui";
+import Dot from "./CarbonDotMark.svg";
 import React from "react";
+import { css, jsx } from "@emotion/core";
 import date from "date-and-time";
 
 /**
@@ -22,9 +24,45 @@ const Post: React.FC<BlogProps> = (props) => {
     <Styled.root data-testid="theme-root">
       <div css={css``}>
         <div>
-          <h3>{props.title}</h3>
-          <p>{date.format(new Date(props.date), "D MMMM Y")}</p>
-          <p>{props.author}</p>
+          <h3
+            css={css`
+              font-size: 2rem;
+              margin-bottom: 15px;
+            `}
+          >
+            {props.title}
+          </h3>
+          <span
+            css={css`
+              display: flex;
+            `}
+          >
+            <p
+              css={css`
+                margin-right: 20px;
+                display: flex;
+
+                &::after {
+                  content: "";
+                  display: fles;
+                  width: 20px;
+                  height: 20px;
+
+                  mask: url(${Dot}) no-repeat 0 0 / contain;
+                  -webkit-mask: url(${Dot}) no-repeat 0 0 / contain;
+                  background-image: unset;
+                  background-color: white;
+                  background-repeat: no-repeat;
+                  background-size: contain;
+                  will-change: mask;
+                  flex-shrink: 0;
+                }
+              `}
+            >
+              {date.format(new Date(props.date), "D MMMM Y")}
+            </p>
+            <p>{props.author}</p>
+          </span>
         </div>
         <div>{props.content}</div>
       </div>
