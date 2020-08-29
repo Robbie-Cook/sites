2d78dc3ec180cfcb213f387633c527f4eadf434c
Robbie Cook <robbieck7@gmail.com>
2020-08-29 17:44:54 +1200

    Starting to implement Blog storybook

--- packages/react-components/src/stories/Button.stories.tsx
+++ packages/react-components/src/stories/Button.stories.tsx
@@ -1,38 +1,23 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import { Button, ButtonProps } from './Button';
+import BlogPost, { BlogProps } from "../blog/BlogPost";
 
 export default {
-  title: 'Example/Button',
-  component: Button,
+  title: "Blog/BlogPost",
+  component: BlogPost,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<BlogProps> = (args) => <BlogPost {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+  title: "My blog post",
+  author: "Robbie Cook",
+  content: "This is some test content",
+  date: Date.now(),
 };
