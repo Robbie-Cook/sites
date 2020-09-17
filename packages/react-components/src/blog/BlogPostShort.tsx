932afb2f30b0c839db6e5aaf4136164fd9774bb3
Robbie Cook <robbieck7@gmail.com>
2020-09-17 12:59:35 +1200

    Fixed build

--- packages/react-components/src/blog/BlogPostShort.tsx
+++ packages/react-components/src/blog/BlogPostShort.tsx
@@ -6,7 +6,7 @@ import { Theme } from "../theme/Theme";
 import BlogInfo from "./BlogInfo";
 import BlogHeader from "./BlogHeader";
 import BlogSection from "./BlogSection";
-import { useTheme } from "../theme/ReactComponentsContext";
+// import { useTheme } from "../theme/ReactComponentsContext";
 
 // @ts-ignore
 import Link from "./link.svg";
