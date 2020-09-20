d8554ebe995c5aa2dc299a4aaef052d438747621
Robbie Cook <robbieck7@gmail.com>
2020-09-20 13:15:54 +1200

    Made each item editor it's own page

--- packages/blog-data/frontend/src/components/App.tsx
+++ packages/blog-data/frontend/src/components/App.tsx
@@ -4,6 +4,7 @@ import { H1 } from "@robbie-cook/react-components";
 import EditorPage from "../pages/EditorPage";
 import FileTile from "./FileTile";
 import FileTileWrapper from "./FileTileWrapper";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 async function fetchFiles() {
   const files = await (await fetch("http://localhost:3001/files")).json();
@@ -22,13 +23,26 @@ function App() {
 
   return (
     <main>
-      {files && !activeFile && (
-        <FileTileWrapper
-          files={files}
-          onFileClick={(file) => setActiveFile(file)}
-        />
-      )}
-      {activeFile && <EditorPage file={activeFile} />}
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {files && !activeFile && (
+              <FileTileWrapper
+                files={files}
+                onFileClick={(file) => {
+                  (window as any).location = `/${file}`;
+                }}
+              />
+            )}
+          </Route>
+          {files &&
+            files.map((file) => (
+              <Route path={`/${file}`}>
+                <EditorPage file={file} />
+              </Route>
+            ))}
+        </Switch>
+      </Router>
     </main>
   );
 }
