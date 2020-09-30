332238e263e1f6c03af3a5df713e4d7238bda22f
Robbie Cook <robbie@cloudcannon.com>
2020-09-30 01:40:53 +0000

    Updated '_cloudcannon-prebuild.sh' via CloudCannon

--- _cloudcannon-prebuild.sh
+++ _cloudcannon-prebuild.sh
@@ -1,12 +1,14 @@
 set -e
 set -o pipefail
 
+npx lerna bootstrap
+
+
 if [ STORYBOOK ]; then
   cd packages/react-components
   npm run build-storybook
 else
   cd packages/personal-website
-  npx lerna bootstrap
   npm run build
 fi
 
