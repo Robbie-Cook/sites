002cb162c796cd9476eae9b25984178d4db77cb9
Robbie Cook <robbieck7@gmail.com>
2020-10-28 18:27:02 +1300

    Made install lerna

--- _cloudcannon-prebuild.sh
+++ _cloudcannon-prebuild.sh
@@ -3,12 +3,12 @@ set -o pipefail
 
 if [ $STORYBOOK ]; then
   cd packages/react-components
-  npm i
+  npx lerna bootstrap
   npm run build-storybook
 else
   cd packages/personal-website
   rm -rf node_modules
-  npm i
+  npx lerna bootstrap
   npm run build
 fi
 
