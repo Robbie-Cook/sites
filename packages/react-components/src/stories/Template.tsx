7be9f48cd11fb2cb36aab6839da730469363c70f
Robbie Cook <robbieck7@gmail.com>
2021-02-10 16:02:25 +1300

    Added openpolitics

---
+++ packages/react-components/src/stories/Template.tsx
@@ -0,0 +1,17 @@
+import ReactComponentsContext from "../theme/ReactComponentsContext";
+import { setTheme } from "bigiron.css";
+
+export default class Template {
+  static createTemplate(args: any, component: () => React.FC<any>) {
+    return (
+      <ReactComponentsContext.Provider
+        value={{
+          type: args.type,
+        }}
+      >
+        {setTheme(args.type)}
+        {component()}
+      </ReactComponentsContext.Provider>
+    );
+  }
+}
