6e24113237bc8c3507ebd4857e283454080b1222
Robbie Cook <robbieck7@gmail.com>
2021-02-25 12:07:24 +1300

    Moved ../cara into ./

--- packages/personal-website-next/pages/_app.tsx
+++ packages/personal-website-next/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
 import "../styles.scss";
-import "../cara/styles/animations";
-import "../cara/styles/utils";
+import "../styles/animations";
+import "../styles/utils";
 import Theme from "../theme-ui";
 import { ThemeProvider } from "theme-ui";
 
