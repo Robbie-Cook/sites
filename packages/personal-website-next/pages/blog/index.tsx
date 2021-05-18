bf572ddf6c36f628ac6280a571f08b858076f6cf
Robbie Cook <robbie.cook@sendpayments.com>
2021-05-18 18:27:45 +1200

    Starting to fill out blog page

---
+++ packages/personal-website-next/pages/blog/index.tsx
@@ -0,0 +1,35 @@
+import { Blog } from "@robbie-cook/react-components";
+import { BlogProps } from "@robbie-cook/react-components/dist/blog/Blog";
+import { getAllPosts, getPostBySlug } from "../../lib/PostHelper";
+import React from "react";
+
+/**
+ * Interface for Blog props
+ */
+export interface BlogPageProps {
+  children?: any;
+  posts?: BlogProps;
+}
+
+/**
+ *  A Blog component.
+ */
+const BlogPage: React.FC<BlogProps> = (props) => {
+  console.log(props.posts);
+  // return <React.Fragment />;
+  return <Blog posts={props.posts} />;
+};
+
+export function getStaticProps(context: any) {
+  const posts = getAllPosts(["slug", "content", "author", "date", "title"]);
+  return {
+    props: {
+      posts: posts.reduce(
+        (acc, curr) => [...acc, { ...curr, date: curr.date?.toString() ?? null }],
+        []
+      ),
+    },
+  };
+}
+
+export default BlogPage;
