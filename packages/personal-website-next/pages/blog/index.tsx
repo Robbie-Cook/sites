dc334bd76821b9353890d7216396f58fd107affc
Robbie Cook <robbie.cook@sendpayments.com>
2021-05-25 12:35:12 +1200

    fix: getting previews

--- packages/personal-website-next/pages/blog/index.tsx
+++ packages/personal-website-next/pages/blog/index.tsx
@@ -1,9 +1,11 @@
 /** @jsx jsx */
 import { Blog } from "@robbie-cook/react-components";
 import { BlogProps } from "@robbie-cook/react-components/dist/blog/Blog";
+import marked from "marked";
 import { getAllPosts, getPostBySlug } from "../../lib/PostHelper";
 import React from "react";
-import { jsx, css } from '@emotion/react';
+import { jsx, css } from "@emotion/react";
+import removeMd from "remove-markdown";
 
 /**
  * Interface for Blog props
@@ -18,11 +20,12 @@ export interface BlogPageProps {
  */
 const BlogPage: React.FC<BlogProps> = (props) => {
   console.log(props.posts);
-  // return <React.Fragment />;
   return (
-    <div css={css`
-      padding: 60px 20%;
-    `}>
+    <div
+      css={css`
+        padding: 60px 20%;
+      `}
+    >
       <Blog posts={props.posts} />
     </div>
   );
@@ -35,7 +38,11 @@ export function getStaticProps(context: any) {
       posts: posts.reduce(
         (acc, curr) => [
           ...acc,
-          { ...curr, date: curr.date?.toString() ?? null },
+          {
+            ...curr,
+            date: curr.date?.toString() ?? null,
+            content: `${removeMd(curr.content).split(' ').slice(0, 60).join(' ')}...`,
+          },
         ],
         []
       ),
