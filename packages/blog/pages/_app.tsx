d3db583f8651bfc1faff1172fdc3d96c7639394a
Robbie Cook <robbie.cook@sendpayments.com>
2021-06-27 23:17:45 +1200

    fix: fixing blog

---
+++ packages/blog/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../styles/globals.css";
+import "../styles.scss";
+import "@robbie-cook/react-components/dist/index.css";
+import Theme from "./theme-ui";
+import Helmet from "react-helmet";
+import { ThemeProvider } from "theme-ui";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={Theme}>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Vollkorn&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
