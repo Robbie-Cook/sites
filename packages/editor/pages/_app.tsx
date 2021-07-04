f3eb34f94fb37ae6e22eec1af179d18b2d554df4
Robbie Cook <robbie.cook@sendpayments.com>
2021-07-04 23:45:10 +1200

    fix: moving markdown editor to own repo

--- packages/editor/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'react-quill/dist/quill.snow.css';
-import 'bigiron.css/dist/bigiron.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
