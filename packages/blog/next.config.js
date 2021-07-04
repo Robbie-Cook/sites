ee9d9e50605cf3d1fcf1af529ca66f74011fb7ae
Robbie Cook <robbie.cook@sendpayments.com>
2021-07-04 21:46:46 +1200

    fix: fix

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -1,4 +1,6 @@
-const withPlugins = require('next-compose-plugins');
+const withPlugins = require("next-compose-plugins");
+
+const CircularDependencyPlugin = require("circular-dependency-plugin");
 
 const withMDX = require("@next/mdx")();
 
@@ -11,6 +13,17 @@ const webpack = (config, options) => {
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   });
+
+  config.plugins.push(
+    new CircularDependencyPlugin({
+      failOnError: true,
+      // allow import cycles that include an asyncronous import,
+      // e.g. via import(/* webpackMode: "weak" */ './file.js')
+      allowAsyncCycles: false,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    })
+  );
 
   return config;
 };
