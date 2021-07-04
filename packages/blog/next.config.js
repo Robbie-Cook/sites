5740e03f172de49fea94b6fcbfaa7bd0569974e3
Robbie Cook <robbie.cook@sendpayments.com>
2021-07-04 21:50:44 +1200

    fix: fix

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -16,14 +16,12 @@ const webpack = (config, options) => {
 
   config.plugins.push(
     new CircularDependencyPlugin({
+      exclude: /node_modules/,
       failOnError: true,
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
       allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
       cwd: process.cwd(),
-    })
-  );
+    }),
+  )
 
   return config;
 };
