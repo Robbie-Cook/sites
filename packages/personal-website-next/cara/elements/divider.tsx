8eff1d089517dea1283904bc657f5ae8fbf1ea0e
Robbie Cook <robbie.cook@sendpayments.com>
2021-07-07 00:26:03 +1200

    fix: fixing skew

--- packages/personal-website-next/cara/elements/divider.tsx
+++ packages/personal-website-next/cara/elements/divider.tsx
@@ -1,23 +1,36 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import { css, jsx } from "theme-ui"
-import React from "react"
-import { ParallaxLayer } from "react-spring/renderprops-addons"
+import { css, jsx } from "theme-ui";
+import React from "react";
+import { ParallaxLayer } from "react-spring/renderprops-addons";
 
 type DividerProps = {
-  speed: number
-  offset: number
-  children?: React.ReactNode
-  bg?: string
-  fill?: string
-  clipPath?: string
-  className?: string
-  factor?: number
-}
+  speed: number;
+  offset: number;
+  children?: React.ReactNode;
+  bg?: string;
+  fill?: string;
+  clipPath?: string;
+  className?: string;
+  factor?: number;
+  style?: React.CSSProperties;
+};
 
-const Divider = ({ speed, offset, factor, bg, fill, clipPath, children, className }: DividerProps) => (
+const Divider = ({
+  speed,
+  offset,
+  factor,
+  bg,
+  fill,
+  clipPath,
+  children,
+  className,
+  style,
+}: DividerProps) => (
   <ParallaxLayer
+    // @ts-ignore
+    style={style}
     css={css({
       position: `absolute`,
       width: `full`,
@@ -36,6 +49,6 @@ const Divider = ({ speed, offset, factor, bg, fill, clipPath, children, classNam
   >
     {children}
   </ParallaxLayer>
-)
+);
 
-export default Divider
+export default Divider;
